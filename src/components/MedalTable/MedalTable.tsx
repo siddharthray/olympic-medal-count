5a6f530d4963158fe259fc047923181b2ef00bb2
Siddharth Ray <siddharth.ray23@gmail.com>
2025-04-04 04:09:04 -0400

    Create medal table component with sorting capabilitie

--- src/components/MedalTable/MedalTable.tsx
+++ src/components/MedalTable/MedalTable.tsx
@@ -1,10 +1,85 @@
 import React from "react";
+import CountryFlag from "../CountryFlag";
+import { MedalWithTotal, SortType } from "../../types/models";
+import { getTopCountries } from "../../utils/sortHelpers";
 import "./MedalTable.css";
 
-// TODO: Implement MedalTable component
+// Props for the MedalTable component
+interface MedalTableProps {
+  medals: MedalWithTotal[];
+  sortBy: SortType;
+  onSortChange: (sortBy: SortType) => void;
+}
 
-const MedalTable: React.FC = () => {
-  return <div>Medal Table Placeholder</div>;
+/**
+ * Component to display the medal count table
+ */
+const MedalTable: React.FC<MedalTableProps> = ({
+  medals,
+  sortBy,
+  onSortChange,
+}) => {
+  const topCountries = getTopCountries(medals, sortBy);
+
+  return (
+    <div className="medal-table">
+      <h1>MEDAL COUNT</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th colSpan={2}></th>
+            <th
+              className={sortBy === "gold" ? "selected" : ""}
+              onClick={() => onSortChange("gold")}
+              style={{ cursor: "pointer" }}
+            >
+              <span className="medal-icon gold"></span>
+            </th>
+            <th
+              className={sortBy === "silver" ? "selected" : ""}
+              onClick={() => onSortChange("silver")}
+              style={{ cursor: "pointer" }}
+            >
+              <span className="medal-icon silver"></span>
+            </th>
+            <th
+              className={sortBy === "bronze" ? "selected" : ""}
+              onClick={() => onSortChange("bronze")}
+              style={{ cursor: "pointer" }}
+            >
+              <span className="medal-icon bronze"></span>
+            </th>
+            <th
+              className={sortBy === "total" ? "selected" : ""}
+              onClick={() => onSortChange("total")}
+              style={{ cursor: "pointer" }}
+            >
+              TOTAL
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {topCountries.map((country, index) => (
+            <tr key={country.code}>
+              <td>{index + 1}</td>
+              <td>
+                <div className="flag-container">
+                  <CountryFlag countryCode={country.code} />
+                </div>
+              </td>
+              <td>{country.code}</td>
+              <td>{country.gold}</td>
+              <td>{country.silver}</td>
+              <td>{country.bronze}</td>
+              <td>{country.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MedalTable;
--- src/constants/countryPositions.ts
+++ src/constants/countryPositions.ts
@@ -1,2 +1,24 @@
-// Constants for country flags positioning
-// TODO: Define country position mapping
+import { CountryCode } from "../types/models";
+
+// The flags are arranged vertically in alphabetical order by country code
+export const COUNTRY_POSITIONS: Record<CountryCode, number> = {
+  AUT: 0,
+  BLR: 1,
+  CAN: 2,
+  CHN: 3,
+  FRA: 4,
+  GER: 5,
+  ITA: 6,
+  NED: 7,
+  NOR: 8,
+  RUS: 9,
+  SUI: 10,
+  SWE: 11,
+  USA: 12,
+};
+
+// Dimensions of each flag in the sprite
+export const FLAG_DIMENSIONS = {
+  width: 28,
+  height: 17,
+};

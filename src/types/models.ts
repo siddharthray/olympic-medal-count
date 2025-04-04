2f9724a331e5c8a2754ca90e055ea1524ce84a5b
Siddharth Ray <siddharth.ray23@gmail.com>
2025-04-04 03:49:16 -0400

    git commit -m "Add medal and country data models"

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,3 +1,37 @@
-// Type definitions for the application
-// TODO: Define Medal and other interfaces
+// Medal data interface
+export interface Medal {
+  code: string;
+  gold: number;
+  silver: number;
+  bronze: number;
+}
+
+// Medal with total count added
+export interface MedalWithTotal extends Medal {
+  total: number;
+}
+
+// Sort types for medal table
 export type SortType = "gold" | "silver" | "bronze" | "total";
+
+// Country flag dimensions
+export interface FlagDimensions {
+  width: number;
+  height: number;
+}
+
+// Country code type for type safety
+export type CountryCode =
+  | "AUT"
+  | "BLR"
+  | "CAN"
+  | "CHN"
+  | "FRA"
+  | "GER"
+  | "ITA"
+  | "NED"
+  | "NOR"
+  | "RUS"
+  | "SUI"
+  | "SWE"
+  | "USA";
